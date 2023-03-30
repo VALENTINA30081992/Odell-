905fa017ccfe1811d23d0d135b3f22b55af97057
My Name <[200~>
2023-03-30 23:56:44 +0300

    init

--- scripts/script.js
+++ scripts/script.js
@@ -1,83 +1,33 @@
-//////////////////////////////////////////////////////////////////////////////
-/* Trees slick*/
-$(".slides").slick({
-  infinite: true,
-  speed: 900,
-  slidesToShow: 4,
-  slidesToScroll: 4,
-  nextArrow:
-    '<button type="button" class="sliders__next"><img src="../images/icons/sliders_prev.svg" alt="prev" /></button>',
-  prevArrow:
-    '<button type="button" class="sliders__prev"><img src="../images/icons/sliders_prev.svg" alt="prev" /></button>',
-  responsive: [
-    {
-      breakpoint: 1920,
-      settings: {
-        slidesToShow: 4,
-        slidesToScroll: 4,
-        infinite: true,
-      },
-    },
-    {
-      breakpoint: 1344,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        infinite: true,
-      },
-    },
-    {
-      breakpoint: 945,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2,
-      },
-    },
-    {
-      breakpoint: 705,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
-      },
-    },
-  ],
-});
-//////////////////////////////////////////////////////////////////////////////
-/* Reviews slick*/
-
-$(".reviews__body").slick({
-  infinite: true,
-  speed: 600,
-  nextArrow:
-    '<button type="button" class="reviews__next"><img src="../images/icons/reviews_prev.svg" alt="prev" /></button>',
-  prevArrow:
-    '<button type="button" class="reviews__prev"><img src="../images/icons/reviews_prev.svg" alt="prev" /></button>',
-});
-
-$("#menu").on("click", function () {
-  $("ul.header__main-list__submenu").slideToggle("fast");
-});
-/////////////////////////////////////////////////////////////////////////////////
-/*FIXED MENU*/
-
-$(document).ready(function () {
-  let $window = $(window),
-    $target = $(".header__wrapper-top .dropdown"),
-    $topPosition = $target.offset().top;
-
-  $window.on("scroll", function () {
-    let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-    if (scrollTop > $topPosition) {
-      $target.addClass("fixed");
-      return;
-    } else {
-      $target.removeClass("fixed");
-    }
-    return;
-  });
-});
-
 ///////////////////////////////////////////////////////////
+jQuery(document).ready(function ($) {
+  // go-top
+
+  $(window).on("scroll", function () {
+    if ($(window).scrollTop() > 800) {
+      $(".back-top").show();
+    } else {
+      $(".back-top").hide();
+    }
+  });
+
+  $(".back-top").on("click", function () {
+    $("html, body").animate({ scrollTop: 0 }, "slow");
+    return false;
+  });
+
+  ////----sub menu
+
+  $(".menu-item-has-children").on("click", function () {
+    if ($(document).width() < 1023) {
+      $(this).children(".sub-menu").slideToggle("fast");
+    }
+  });
+
+  ///////////////////////////////////////////////////////////////////////////////////* Set current year*/
+  const yearEl = document.querySelector(".year");
+  const currentYear = new Date().getFullYear();
+  yearEl.textContent = currentYear;
+});
 /* Make mobile navigation work*/
 
 const btnNavEl = document.querySelector(".btn-mobile-nav");
@@ -97,19 +47,3 @@ window.onscroll = () => {
     goTop.style.display = "block";
   }
 };
-///////////////////////////////////////////////////////////////////////////////////* Set current year*/
-const yearEl = document.querySelector(".year");
-const currentYear = new Date().getFullYear();
-yearEl.textContent = currentYear;
-
-///----pop-up
-function PopUpShow() {
-  $(".popup").show();
-}
-function PopUpHide() {
-  $(".popup").hide();
-}
-
-$(".popup").on("click", function () {
-  PopUpHide();
-});
