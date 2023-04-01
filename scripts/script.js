db27633e0939e693beeb7bf84a34174cd1d3d38c
My Name <[200~>
2023-04-01 13:15:07 +0300

    init

--- scripts/script.js
+++ scripts/script.js
@@ -15,35 +15,16 @@ jQuery(document).ready(function ($) {
     return false;
   });
 
-  ////----sub menu
-
-  $(".menu-item-has-children").on("click", function () {
-    if ($(document).width() < 1023) {
-      $(this).children(".sub-menu").slideToggle("fast");
-    }
-  });
-
   ///////////////////////////////////////////////////////////////////////////////////* Set current year*/
   const yearEl = document.querySelector(".year");
   const currentYear = new Date().getFullYear();
   yearEl.textContent = currentYear;
+
+  ////----sub menu
+
+  $(".menu-item-has-children").on("click", function () {
+    if ($(document).width() < 944) {
+      $(this).children(".sub-menu").slideToggle("fast");
+    }
+  });
 });
-/* Make mobile navigation work*/
-
-const btnNavEl = document.querySelector(".btn-mobile-nav");
-const headerEl = document.querySelector(".dropdown");
-
-btnNavEl.addEventListener("click", function () {
-  headerEl.classList.toggle("nav-open");
-});
-
-/////////////////////////////////////////////////////
-/*Scroll back to top*/
-const goTop = document.querySelector(".back-top");
-window.onscroll = () => {
-  if (window.pageYOffset < 700) {
-    goTop.style.display = "none";
-  } else {
-    goTop.style.display = "block";
-  }
-};
